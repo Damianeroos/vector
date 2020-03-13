7ec3bc44b340a971440104fddd1d9e8c3de9cb41
Damian Rakowski <damian.rakowski93@o2.pl>
2020-03-13 11:08:30 +0100

    added tests

--- test_vector.cpp
+++ test_vector.cpp
@@ -1,82 +1,85 @@
 #include "Vector.hpp"
 
-//#include <vector>
-#include <cassert>
-#include <stdexcept>
+
+// #include <cassert>
+// #include <stdexcept>
 #include <string>
+#include <gtest/gtest.h>
 
-// template <typename T>
-// using Vector = std::vector<T>;
 
-int main() {
+TEST(VectorTest, emptyFunction){
+  Vector<int> i;
+  Vector<float> f;
+  Vector<double> d;
+  Vector<std::string> s;
+  EXPECT_TRUE(i.empty());
+  EXPECT_TRUE(f.empty());
+  EXPECT_TRUE(d.empty());
+  EXPECT_TRUE(s.empty());
 
-  {
-    Vector<int> i;
-    Vector<float> f;
-    Vector<double> d;
-    Vector<std::string> s;
+}
 
-    assert(i.empty());
-    assert(f.empty());
-    assert(d.empty());
-    assert(s.empty());
+TEST(VectorTest, SizeCopyFnc){
+  Vector<int> a = {1, 2, 3};
+  assert(a.size() == 3);
+  EXPECT_TRUE(a.size()==3);
+  Vector<int> copy = a;
+  EXPECT_TRUE(a.size() == 3);
+  EXPECT_TRUE(copy.size() == 3);
+
+}
+
+TEST(VectorTest, accessOperators){
+  Vector<int> b(3);
+  EXPECT_TRUE(b.size() == 3);
+
+  EXPECT_TRUE(b[0] == 0);
+  EXPECT_TRUE(b[1] == 0);
+  EXPECT_TRUE(b[2] == 0);
+
+  EXPECT_TRUE(b.at(0) == 0);
+  EXPECT_TRUE(b.at(1) == 0);
+  EXPECT_TRUE(b.at(2) == 0);
+
+  bool thrown = false;
+  try {
+    b.at(3);
+  } catch (std::out_of_range &) {
+    thrown = true;
   }
+  EXPECT_TRUE(thrown);
+}
 
-  {
-    Vector<int> a = {1, 2, 3};
-    assert(a.size() == 3);
-    Vector<int> copy = a;
-    assert(a.size() == 3);
-    assert(copy.size() == 3);
-  }
+TEST(VectorTest, elemAdding){
+  Vector<int> c = {1, 2, 3};
 
-  {
-    Vector<int> b(3);
-    assert(b.size() == 3);
+  c.push_back(99);
+  EXPECT_TRUE(c.size() == 4);
+  EXPECT_TRUE(c.back() == 99);
 
-    assert(b[0] == 0);
-    assert(b[1] == 0);
-    assert(b[2] == 0);
+  c.back() = 100;
+  EXPECT_TRUE(c.back() == 100);
+  c.back()++;
+  EXPECT_TRUE(c.back() == 101);
 
-    assert(b.at(0) == 0);
-    assert(b.at(1) == 0);
-    assert(b.at(2) == 0);
+  c.pop_back();
+  EXPECT_TRUE(c.size() == 3);
+  EXPECT_TRUE(c.back() == 3);
+}
 
-    bool thrown = false;
-    try {
-      b.at(3);
-    } catch (std::out_of_range &) {
-      thrown = true;
-    }
-    assert(thrown);
-  }
+TEST(VectorTest, assignmentOperator){
+  Vector<int> at = {100, 2, 3, 50};
+  Vector<int> bt = {1, 2, 3};
+  Vector<int> ct = {1, 2, 3};
+  EXPECT_TRUE(at != bt);
+  EXPECT_TRUE(bt == ct);
 
-  {
-    Vector<int> c = {1, 2, 3};
+  at = bt;
+  EXPECT_TRUE(at == bt);
+  EXPECT_TRUE(at.front() == 1 && bt.front() == 1);
+}
 
-    c.push_back(99);
-    assert(c.size() == 4);
-    assert(c.back() == 99);
-
-    c.back() = 100;
-    assert(c.back() == 100);
-    c.back()++;
-    assert(c.back() == 101);
-
-    c.pop_back();
-    assert(c.size() == 3);
-    assert(c.back() == 3);
-  }
-
-  {
-    Vector<int> at = {100, 2, 3, 50};
-    Vector<int> bt = {1, 2, 3};
-    Vector<int> ct = {1, 2, 3};
-    assert(at != bt);
-    assert(bt == ct);
-
-    at = bt;
-    assert(at == bt);
-    assert(at.front() == 1 && bt.front() == 1);
-  }
+int main(int argc,char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS(); 
 }

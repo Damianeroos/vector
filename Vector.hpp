3e14938dc863c6a6bb3f71b500e190f92d9f65c0
Damian Rakowski <damian.rakowski93@o2.pl>
2020-03-16 11:46:56 +0100

    extended tests

--- Vector.hpp
+++ Vector.hpp
@@ -7,8 +7,9 @@
 #include <stdexcept>
 #include <type_traits>
 
-template <class T> class Vector {
-public:
+template <class T>
+class Vector {
+ public:
   Vector() : m_size(0) {
     m_entities = std::make_unique<T[]>(4);
     m_real_size = 4;
@@ -34,53 +35,53 @@ public:
   bool operator!=(const Vector<T> &arg) const { return !(*this == arg); }
   Vector<T> &operator=(const Vector<T> &);
 
-  auto begin() { return &m_entities[0]; }
-  auto begin() const { return &m_entities[0]; }
-  auto end() { return &m_entities[m_size]; }
-  auto end() const { return &m_entities[m_size]; }
+  T *begin() { return &m_entities[0]; }
+  T *begin() const { return &m_entities[0]; }
+  T *end() { return &m_entities[m_size]; }
+  T *end() const { return &m_entities[m_size]; }
 
   size_t compute_pow(const size_t &size) const;
 
-private:
+ private:
   std::unique_ptr<T[]> m_entities;
   std::size_t m_size;
   std::size_t m_real_size;
 };
 
-template <class T> Vector<T>::Vector(std::initializer_list<T> list) {
+template <class T>
+Vector<T>::Vector(std::initializer_list<T> list) {
   if (!list.size()) {
     return;
   }
-  std::size_t temp_real_size = 1 << compute_pow(list.size());
-  auto temp_entities = std::make_unique<T[]>(temp_real_size);
-  std::copy(list.begin(), list.end(), &temp_entities[0]);
 
-  m_entities = std::move(temp_entities);
-  m_real_size = temp_real_size;
+  m_real_size = 1 << compute_pow(list.size());
+  m_entities = std::make_unique<T[]>(m_real_size);
+  std::copy(list.begin(), list.end(), begin());
   m_size = list.size();
 }
 
-template <class T> Vector<T>::Vector(std::size_t arg) {
+template <class T>
+Vector<T>::Vector(std::size_t arg) {
   m_real_size = 1 << compute_pow(arg);
   m_size = arg;
   m_entities = std::make_unique<T[]>(m_real_size);
   std::fill(begin(), end(), T{});
 }
 
-template <class T> T &Vector<T>::at(std::size_t index) {
-  if (index > m_size - 1)
-    throw std::out_of_range("range_check");
+template <class T>
+T &Vector<T>::at(std::size_t index) {
+  if (index > m_size - 1) throw std::out_of_range("range_check");
   return m_entities[index];
 }
 
-template <class T> const T &Vector<T>::at(std::size_t index) const {
-  if (index > m_size - 1)
-    throw std::out_of_range("range_check");
+template <class T>
+const T &Vector<T>::at(std::size_t index) const {
+  if (index > m_size - 1) throw std::out_of_range("range_check");
   return m_entities[index];
 }
 
-template <class T> void Vector<T>::push_back(const T &arg) {
-
+template <class T>
+void Vector<T>::push_back(const T &arg) {
   if (m_real_size == m_size) {
     std::size_t temp_real_size = m_real_size * 2;
 
@@ -97,16 +98,20 @@ template <class T> void Vector<T>::push_back(const T &arg) {
   }
 }
 
-template <class T> void Vector<T>::pop_back() { --m_size; }
+template <class T>
+void Vector<T>::pop_back() {
+  --m_size;
+}
 
-template <class T> bool Vector<T>::operator==(const Vector<T> &arg) const {
-  if (arg.size() != m_size)
-    return false;
+template <class T>
+bool Vector<T>::operator==(const Vector<T> &arg) const {
+  if (arg.size() != m_size) return false;
 
   return (std::equal(begin(), end(), arg.begin(), arg.end()));
 }
 
-template <class T> Vector<T> &Vector<T>::operator=(const Vector<T> &arg) {
+template <class T>
+Vector<T> &Vector<T>::operator=(const Vector<T> &arg) {
   size_t temp_real_size = 1 << compute_pow(arg.size());
 
   auto temp_entities = std::make_unique<T[]>(temp_real_size);
@@ -119,10 +124,10 @@ template <class T> Vector<T> &Vector<T>::operator=(const Vector<T> &arg) {
   return *this;
 }
 
-template <class T> size_t Vector<T>::compute_pow(const size_t &size) const {
+template <class T>
+size_t Vector<T>::compute_pow(const size_t &size) const {
   int p = 0;
   int temp_size = static_cast<int>(size);
-  while (temp_size >= (1 << p))
-    ++p;
+  while (temp_size >= (1 << p)) ++p;
   return p;
 }
--- test_vector.cpp
+++ test_vector.cpp
@@ -1,85 +1,156 @@
+#include <gtest/gtest.h>
+#include <stdexcept>
+#include <string>
 #include "Vector.hpp"
 
-
-// #include <cassert>
-// #include <stdexcept>
-#include <string>
-#include <gtest/gtest.h>
-
-
-TEST(VectorTest, emptyFunction){
+TEST(VectorTest, emptyFunction) {
+  // Given
   Vector<int> i;
   Vector<float> f;
   Vector<double> d;
   Vector<std::string> s;
+
+  // Then
   EXPECT_TRUE(i.empty());
   EXPECT_TRUE(f.empty());
   EXPECT_TRUE(d.empty());
   EXPECT_TRUE(s.empty());
-
 }
 
-TEST(VectorTest, SizeCopyFnc){
+TEST(VectorTest, SizeCopyFnc) {
+  // Given
   Vector<int> a = {1, 2, 3};
-  assert(a.size() == 3);
-  EXPECT_TRUE(a.size()==3);
-  Vector<int> copy = a;
-  EXPECT_TRUE(a.size() == 3);
-  EXPECT_TRUE(copy.size() == 3);
+  Vector<int> copy;
 
+  // When
+  copy = a;
+
+  // Then
+  EXPECT_EQ(a.size(), 3);
+  EXPECT_EQ(copy.size(), 3);
 }
 
-TEST(VectorTest, accessOperators){
+TEST(VectorTest, size_constructor) {
+  // Given
   Vector<int> b(3);
-  EXPECT_TRUE(b.size() == 3);
 
-  EXPECT_TRUE(b[0] == 0);
-  EXPECT_TRUE(b[1] == 0);
-  EXPECT_TRUE(b[2] == 0);
-
-  EXPECT_TRUE(b.at(0) == 0);
-  EXPECT_TRUE(b.at(1) == 0);
-  EXPECT_TRUE(b.at(2) == 0);
-
-  bool thrown = false;
-  try {
-    b.at(3);
-  } catch (std::out_of_range &) {
-    thrown = true;
-  }
-  EXPECT_TRUE(thrown);
+  // Then
+  EXPECT_EQ(b.size(), 3);
 }
 
-TEST(VectorTest, elemAdding){
+TEST(VectorTest, subscript_operator) {
+  // Given
+  Vector<int> b(4);
+
+  // Then
+  EXPECT_EQ(b[0], 0);
+  EXPECT_EQ(b[1], 0);
+  EXPECT_EQ(b[2], 0);
+  EXPECT_EQ(b[3], 0);
+}
+
+TEST(VectorTest, at_function) {
+  // Given
+  Vector<int> b(4);
+
+  // Then
+  EXPECT_EQ(b.at(0), 0);
+  EXPECT_EQ(b.at(1), 0);
+  EXPECT_EQ(b.at(2), 0);
+  EXPECT_EQ(b.at(3), 0);
+  EXPECT_THROW(b.at(4), std::out_of_range);
+}
+
+TEST(VectorTest, elemAdding) {
+  // Given
   Vector<int> c = {1, 2, 3};
 
+  // When
   c.push_back(99);
-  EXPECT_TRUE(c.size() == 4);
-  EXPECT_TRUE(c.back() == 99);
 
-  c.back() = 100;
-  EXPECT_TRUE(c.back() == 100);
-  c.back()++;
-  EXPECT_TRUE(c.back() == 101);
-
-  c.pop_back();
-  EXPECT_TRUE(c.size() == 3);
-  EXPECT_TRUE(c.back() == 3);
+  // Then
+  EXPECT_EQ(c.size(), 4);
 }
 
-TEST(VectorTest, assignmentOperator){
+TEST(VectorTest, back_function) {
+  // Given
+  Vector<int> c = {1, 2, 3};
+
+  // When
+  c.push_back(99);
+  c.back()++;
+
+  // Then
+  EXPECT_EQ(c.back(), 100);
+}
+
+TEST(VectorTest, front_function) {
+  // Given
+  Vector<int> c = {665, 2, 3};
+
+  // When
+  c.front()++;
+
+  // Then
+  EXPECT_EQ(c.front(), 666);
+}
+
+TEST(VectorTest, pop_back_function) {
+  // Given
+  Vector<int> c = {1, 2, 3};
+
+  // When
+  c.pop_back();
+
+  // Then
+  EXPECT_EQ(c.back(), 2);
+  EXPECT_EQ(c.size(), 2);
+}
+
+TEST(VectorTest, relational_operator) {
+  // Given
   Vector<int> at = {100, 2, 3, 50};
   Vector<int> bt = {1, 2, 3};
   Vector<int> ct = {1, 2, 3};
-  EXPECT_TRUE(at != bt);
-  EXPECT_TRUE(bt == ct);
+  Vector<int> dt = {1, 2, 3};
 
-  at = bt;
-  EXPECT_TRUE(at == bt);
-  EXPECT_TRUE(at.front() == 1 && bt.front() == 1);
+  // When
+  dt = at;
+
+  // Then
+  EXPECT_EQ(dt, at);
+  EXPECT_NE(at, bt);
+  EXPECT_EQ(bt, ct);
+  EXPECT_EQ(at.front(), dt.front());
 }
 
-int main(int argc,char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS(); 
+TEST(VectorTest, compute_pow_fnc) {
+  // Given
+  Vector<int> a;
+
+  // Then
+  EXPECT_EQ(a.compute_pow(3), 2);
+  EXPECT_EQ(a.compute_pow(4), 3);
+  EXPECT_EQ(a.compute_pow(5), 3);
+  EXPECT_EQ(a.compute_pow(0), 0);
+  EXPECT_EQ(a.compute_pow(-1), 0);
+}
+
+TEST(VectorTest, begin_iterator) {
+  // Given
+  Vector<int> a = {1, 2, 3};
+
+  // When
+  *a.begin() = 9;
+
+  // Then
+  EXPECT_EQ(a.front(), 9);
+}
+
+TEST(VectorTest, end_iterator) {
+  // Given
+  Vector<int> a;
+
+  // Then
+  EXPECT_EQ(*a.end(), 0);
 }
